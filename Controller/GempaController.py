fab09a8df952f17fcb7351690e8ab1bf89f3b375
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-07 08:52:58 +0000

    Add def for routes.

--- Controller/GempaController.py
+++ Controller/GempaController.py
@@ -1,7 +1,7 @@
 import requests, xmltodict
 
 
-def GempaNews():
+def datagempa():
   url = "https://bmkg-content-inatews.storage.googleapis.com/datagempa.json"
   response = requests.get(url)
   if response.status_code == 200:
@@ -13,23 +13,30 @@ def GempaNews():
 def last30event():
   url = "https://bmkg-content-inatews.storage.googleapis.com/last30event.xml"
   response = requests.get(url)
-  data_dict = xmltodict.parse(response.content)
+  xml_data = response.content
+  data_dict = xmltodict.parse(xml_data)
   return data_dict
 
 
 def last30feltevent():
   url = "https://bmkg-content-inatews.storage.googleapis.com/last30feltevent.xml"
   response = requests.get(url)
-  return response.content
+  xml_data = response.content
+  data_dict = xmltodict.parse(xml_data)
+  return data_dict
 
 
 def last30tsunamievent():
   url = "https://bmkg-content-inatews.storage.googleapis.com/last30tsunamievent.xml"
   response = requests.get(url)
-  return response.content
+  xml_data = response.content
+  data_dict = xmltodict.parse(xml_data)
+  return data_dict
 
 
 def live30event():
   url = "https://bmkg-content-inatews.storage.googleapis.com/live30event.xml"
   response = requests.get(url)
-  return response.content
+  xml_data = response.content
+  data_dict = xmltodict.parse(xml_data)
+  return data_dict
