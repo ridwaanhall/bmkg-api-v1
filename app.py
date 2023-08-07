340cd006e3e24029f78ecf6c8ff8f86e544ba791
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-07 08:54:14 +0000

    Add routes for xml and json
    m5, felt, stunami, realtime

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, Response
-from Controller.GempaController import GempaNews, last30event, last30feltevent, last30tsunamievent, live30event
-import json, xmltodict
+from Controller.GempaController import datagempa, last30event, last30feltevent, last30tsunamievent, live30event
+import xmltodict
 
 app = Flask(__name__)
 
@@ -10,11 +10,11 @@ def hello_world():
   return "hehehe"
 
 
-# Route to get earthquake data as XML
+# GEMPA TERBARU (from json to xml)
 @app.route('/gempa-terkini.xml')
 @app.route('/datagempa.xml')
 def datagempa_xml():
-  data = GempaNews()
+  data = datagempa()
   if data:
     # Wrap the data in a root element named "root"
     wrapped_data = {"root": data}
@@ -24,50 +24,82 @@ def datagempa_xml():
     return jsonify({"message": "Failed to fetch data."}), 500
 
 
-# Route to get earthquake data as JSON
 @app.route('/gempa-terkini.json')
 @app.route('/datagempa.json')
 def datagempa_json():
-  data = GempaNews()
+  data = datagempa()
   if data:
     return jsonify(data)
   else:
     return jsonify({"message": "Failed to fetch data."}), 500
 
 
-# gempa yang lebih dari 5 Magnitudo
-@app.route('/m5')
-@app.route('/last30event')
-def show_last30event():
+# GEMPA LEBIH DARI SAMADENGAN 5 MAG (from xml to xml)
+@app.route('/m5.xml')
+@app.route('/last30event.xml')
+def last30event_xml():
+  data = last30event()
+  response = Response(xmltodict.unparse(data, pretty=True),
+                      content_type='application/xml')
+  return response
+
+
+@app.route('/m5.json')
+@app.route('/last30event.json')
+def last30event_json():
   data = last30event()
   return jsonify(data)
 
 
-# gempa yang dirasakan
+# GEMPA YANG DIRASAKAN
 @app.route('/felt.xml')
 @app.route('/last30feltevent.xml')
-def show_last30feltevent():
+def last30feltevent_xml():
   data = last30feltevent()
-  response = Response(data, content_type='application/xml')
+  response = Response(xmltodict.unparse(data, pretty=True),
+                      content_type='application/xml')
   return response
 
 
-@app.route('/tsunami')
-@app.route('/last30tsunamievent')
-def show_last30tsunamievent():
+@app.route('/felt.json')
+@app.route('/last30feltevent.json')
+def last30feltevent_json():
+  data = last30feltevent()
+  return jsonify(data)
+
+
+# GEMPA YANG KEMUNGKINAN STUNAMI
+@app.route('/tsunami.xml')
+@app.route('/last30tsunamievent.xml')
+def last30tsunamievent_xml():
   data = last30tsunamievent()
-  # Create a Response object with the XML content and set the content-type header
-  response = Response(data, content_type='application/xml')
+  response = Response(xmltodict.unparse(data, pretty=True),
+                      content_type='application/xml')
   return response
 
 
-@app.route('/realtime')
-@app.route('/live30event')
-def show_live30event():
+@app.route('/tsunami.json')
+@app.route('/last30tsunamievent.json')
+def last30tsunamievent_json():
+  data = last30tsunamievent()
+  return jsonify(data)
+
+
+# GEMPA REAL-TIME
+@app.route('/realtime.xml')
+@app.route('/live30event.xml')
+def live30event_xml():
   data = live30event()
-  # Create a Response object with the XML content and set the content-type header
-  response = Response(data, content_type='application/xml')
+  response = Response(xmltodict.unparse(data, pretty=True),
+                      content_type='application/xml')
   return response
+
+
+@app.route('/realtime.json')
+@app.route('/live30event.json')
+def live30event_json():
+  data = live30event()
+  return jsonify(data)
 
 
 if __name__ == '__main__':
