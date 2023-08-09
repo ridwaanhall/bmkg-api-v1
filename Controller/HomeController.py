1f28b8c02b08dc364edf28404dbc9edd10b67980
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-09 05:36:21 +0000

    Add class HomeController

--- Controller/HomeController.py
+++ Controller/HomeController.py
@@ -1,7 +1,16 @@
-import json
+import json, xmltodict
+from flask import Response
 
+class HomeController:
 
-def load_home_json():
-  with open('datahome/home.json', 'r') as json_file:
-    data = json.load(json_file)
-  return data
+  @staticmethod
+  def load_home_json():
+    with open('datahome/home.json', 'r') as json_file:
+      data = json.load(json_file)
+    return data
+
+  @staticmethod
+  def load_home_xml():
+    data = HomeController.load_home_json()
+    xml_data = xmltodict.unparse({"data": data}, pretty=True)
+    return Response(xml_data, content_type='text/xml')
