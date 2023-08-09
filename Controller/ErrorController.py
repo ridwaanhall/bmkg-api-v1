55f386a6febe894e954d049ef2c75bc028c26dee
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-09 14:01:33 +0000

    Add input to database

--- Controller/ErrorController.py
+++ Controller/ErrorController.py
@@ -1,4 +1,6 @@
 from flask import jsonify, request
+from replit import db
+from datetime import datetime
 
 
 class ErrorController:
@@ -6,8 +8,18 @@ class ErrorController:
   @staticmethod
   def unavailable(path):
     requester_ip = request.remote_addr
+    now = datetime.now()
     response = {
       "message": f"The requested route '{path}' is unavailable.",
-      "ip": requester_ip
+      "ip": requester_ip,
+      "datetime": now
     }
+    del db["IP_address"]
+    del db["path"]
+    del db["datetime"]
+
+    keys = db.keys
+
+    for key in keys:
+      print(f"""{key} : {db["key"]}""")
     return jsonify(response), 404
